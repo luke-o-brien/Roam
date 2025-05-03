129d9554b3ba380befc757560da385d31eac565a
luke-obrien <lukeobrien02@gmail.com>
2025-05-03 13:24:01 +0100

    adjusted pipeline to build and updated .env variable for base url

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Has the name of the correct city', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('/');
   await expect(page.getByTestId('city-name')).toHaveText("London")
 });
 
